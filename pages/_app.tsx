6d0da8005d2b754629626134843ffb4c59d4b667
Ian <iannnop@gmail.com>
2022-03-25 19:56:37 +1000

    moved header to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   )
