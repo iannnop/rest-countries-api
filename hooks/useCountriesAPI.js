c4a0ae1518d7f0ea98bd9821f67d0b99e610a427
Ian <iannnop@gmail.com>
2022-03-25 19:55:37 +1000

    changed to restcountries v2 for border data

--- hooks/useCountriesAPI.js
+++ hooks/useCountriesAPI.js
@@ -3,7 +3,7 @@ import useSWR from "swr"
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const useCountriesAPI = () => {
-  const url = "https://restcountries.com/v3.1/all"
+  const url = "https://restcountries.com/v2/all"
 
   const { data, error } = useSWR(url, fetcher)
 
