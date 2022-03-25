b83d48672caa965f59ac3e1bf0604dc643f8bf5c
Ian <iannnop@gmail.com>
2022-03-25 19:56:16 +1000

    changed type definitions

--- types.d.ts
+++ types.d.ts
@@ -1,105 +1,64 @@
 export interface Country {
-  name: Name;
-  tld?: (string)[] | null;
-  cca2: string;
-  ccn3: string;
-  cca3: string;
-  independent: boolean;
-  status: string;
-  unMember: boolean;
-  currencies: Currencies;
-  idd: Idd;
-  capital?: (string)[] | null;
-  altSpellings?: (string)[] | null;
-  region: string;
-  subregion: string;
-  languages: Languages;
-  translations: Translations;
-  latlng?: (number)[] | null;
-  landlocked: boolean;
-  area: number;
-  demonyms: Demonyms;
-  flag: string;
-  maps: Maps;
-  population: number;
-  car: Car;
-  timezones?: (string)[] | null;
-  continents?: (string)[] | null;
-  flags: FlagsOrCoatOfArms;
-  coatOfArms: FlagsOrCoatOfArms;
-  startOfWeek: string;
-  capitalInfo: CapitalInfo;
+  name:           string;
+  topLevelDomain: string[];
+  alpha2Code:     string;
+  alpha3Code:     string;
+  callingCodes:   string[];
+  capital:        string;
+  altSpellings:   string[];
+  subregion:      string;
+  region:         string;
+  population:     number;
+  latlng:         number[];
+  demonym:        string;
+  area:           number;
+  timezones:      string[];
+  borders:        string[];
+  nativeName:     string;
+  numericCode:    string;
+  flags:          Flags;
+  currencies:     Currency[];
+  languages:      Language[];
+  translations:   Translations;
+  flag:           string;
+  regionalBlocs:  RegionalBloc[];
+  cioc:           string;
+  independent:    boolean;
 }
-export interface Name {
-  common: string;
-  official: string;
-  nativeName: NativeName;
-}
-export interface NativeName {
-  swe: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-}
-export interface SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho {
-  official: string;
-  common: string;
-}
-export interface Currencies {
-  EUR: EUR;
-}
-export interface EUR {
-  name: string;
+
+export interface Currency {
+  code:   string;
+  name:   string;
   symbol: string;
 }
-export interface Idd {
-  root: string;
-  suffixes?: (string)[] | null;
-}
-export interface Languages {
-  swe: string;
-}
-export interface Translations {
-  ara: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  ces: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  cym: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  deu: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  est: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  fin: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  fra: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  hrv: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  hun: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  ita: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  jpn: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  kor: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  nld: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  per: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  pol: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  por: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  rus: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  slk: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  spa: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  swe: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  urd: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-  zho: SweOrAraOrCesOrCymOrDeuOrEstOrFinOrFraOrHrvOrHunOrItaOrJpnOrKorOrNldOrPerOrPolOrPorOrRusOrSlkOrSpaOrUrdOrZho;
-}
-export interface Demonyms {
-  eng: EngOrFra;
-  fra: EngOrFra;
-}
-export interface EngOrFra {
-  f: string;
-  m: string;
-}
-export interface Maps {
-  googleMaps: string;
-  openStreetMaps: string;
-}
-export interface Car {
-  signs?: (string)[] | null;
-  side: string;
-}
-export interface FlagsOrCoatOfArms {
-  png: string;
+
+export interface Flags {
   svg: string;
+  png: string;
 }
-export interface CapitalInfo {
-  latlng?: (number)[] | null;
+
+export interface Language {
+  iso639_1:   string;
+  iso639_2:   string;
+  name:       string;
+  nativeName: string;
+}
+
+export interface RegionalBloc {
+  acronym: string;
+  name:    string;
+}
+
+export interface Translations {
+  br: string;
+  pt: string;
+  nl: string;
+  hr: string;
+  fa: string;
+  de: string;
+  es: string;
+  fr: string;
+  ja: string;
+  it: string;
+  hu: string;
 }
